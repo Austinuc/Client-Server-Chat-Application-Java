b4a7a9c577db7b0b81d2caea0ae989331a7d068f
Austin <augustine.igboke@decagon.dev>
2022-10-26 14:04:41 +0100

    Added Tests

--- src/test/java/ClientServerCommunicationTest.java
+++ src/test/java/ClientServerCommunicationTest.java
@@ -1,56 +1,58 @@
 import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.number.OrderingComparison.greaterThan;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 import java.io.*;
 import java.net.*;
-import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 public class ClientServerCommunicationTest {
 
     private static Socket clientSocket;
-    private static InputStreamReader responseFromServer;
+    private static BufferedReader responseFromServer;
     private static PrintWriter sendMessageToServer;
     private static Socket serverSocket;
     private static final int PORT_NUMBER = 8854;
+    String[] messageToServer;
 
-    @BeforeClass
-    public static void init() throws IOException {
+
+    @Before
+    public void init() throws IOException {
+        messageToServer = new String[]{"Hello Server!", "Are you listening to me?", "I want to speak with you"};
+
         ServerSocket sSocket = new ServerSocket(PORT_NUMBER);
         clientSocket = new Socket("localhost", PORT_NUMBER);
-        responseFromServer = new InputStreamReader(clientSocket.getInputStream());
+        responseFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
         serverSocket = sSocket.accept();
         OutputStream os = serverSocket.getOutputStream();
         sendMessageToServer = new PrintWriter(os, true);
 
+
     }
 
     @Test
     public void testSendingMessageToServerAndPrintingTheResponseFromServer() throws IOException {
-
-        String[] pb = {"Hello Server!","Are you listening to me?","I want to speak with you"};
-
-        for (String lines : pb) {
-            System.out.println(lines);
-            sendMessageToServer.write(lines);
+        for (String lines : messageToServer) {
+            sendMessageToServer.println(lines);
         }
         sendMessageToServer.flush();
-        System.out.println((responseFromServer));
 
-//        assertEquals(responseFromServer,"Hello");
+        String response = "";
+        int lineNumbers = 0;
+        for (; lineNumbers < messageToServer.length; lineNumbers++) {
+            response = responseFromServer.readLine();
 
-//        while (responseFromServer.available() > 0) {
-//            System.out.println(Arrays.toString(responseFromServer.readAllBytes()));
-//        }
+            //validate that each response from the server corresponds to the input line
+            assertThat(response, is(equalTo(messageToServer[lineNumbers])));
+
+            System.out.println(response);
+        }
+        assertThat(lineNumbers,is(equalTo(messageToServer.length)));
     }
 
     @After
